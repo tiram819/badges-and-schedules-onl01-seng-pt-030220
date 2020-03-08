ec2673f2e558406b5beb42ba34fb87d572a98ee4
Tabassum Hassan <tiram819@gmail.com>
2020-03-08 20:06:41 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,3 +12,11 @@ def assign_rooms(attendees)
  attendees.each_with_index.map {|speaker, index| "Hello, #{speaker}! You'll be assigned to room #{index+1}!"}
 end
 
+def printer(speakers)
+ batch_badge_creator(speakers).each do |badge|
+  puts badge
+ end
+ assign_rooms(speakers).each do |assignment|
+  puts assignment
+ end
+end

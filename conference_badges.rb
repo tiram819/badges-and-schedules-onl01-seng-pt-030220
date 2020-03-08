f381e364189089bfed3a77d7071e43060203819f
Tabassum Hassan <tiram819@gmail.com>
2020-03-08 19:41:46 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,4 +2,6 @@ def badge_maker(speaker)
  return "Hello, my name is #{speaker}."
 end
 
-speakers_attendees = 
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def batch_badge_creator(attendees)
